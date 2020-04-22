a59b360cee99b4cf58270f108c267fa36ff02d87
m33erick <m33erick@gmail.com>
2020-04-22 12:15:35 +0200

    crud postman

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,6 +4,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([44.8333, -0.5667]).addTo(map);
-L.marker([44.8533, -0.5667]).addTo(map);
-L.marker([44.8333, -0.5867]).addTo(map);
+// L.marker([44.8333, -0.5667]).addTo(map);
+// L.marker([44.8533, -0.5667]).addTo(map);
+// L.marker([44.8333, -0.5867]).addTo(map);
+
+$.ajax({
+	datatype: "json",
+	url: "api/bicicletas",
+	success: function(result){
+		console.log(result);
+		result.bicicletas.forEach(function(bici) {
+			L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+			
+		});
+	}
+})
